dcdd683575d656962349724eea968530e80f2229
stevenszopinski-gif <stevenszopinski-gif@users.noreply.github.com>
2026-03-01 23:19:12 +0000

    deploy: stevenszopinski-gif/Tailor-Me-Swiftly@bcb825709fccfd8b8c5dbffe3bd5519f5da98928

--- auth.js
+++ auth.js
@@ -100,8 +100,7 @@
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json',
-                            'Authorization': 'Bearer ' + token,
-                            'apikey': token
+                            'Authorization': 'Bearer ' + token
                         },
                         body: JSON.stringify(opts?.body || {})
                     });
